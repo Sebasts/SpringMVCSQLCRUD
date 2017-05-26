6732d02f2ad6bf357fa1855adabb21800a03e49a
Sebastian Hellsing <sebastsing@gmail.com>
2017-05-26 11:27:22 -0600

    Begining of mysql integration with the app

--- src/main/java/io/hellsing/data/PersistenceDAOImpl.java
+++ src/main/java/io/hellsing/data/PersistenceDAOImpl.java
@@ -21,140 +21,42 @@ public class PersistenceDAOImpl implements PersistenceDAO {
 	@Autowired
 	private ServletContext context;
 	
-	private static final String ACCOUNTDB="WEB-INF/accounts.csv";
-	private static final String ACCOUNTDATA="WEB-INF/accountData.csv";
-
-	@Override
-	public void writeNewUserToFile(User user) {
-		System.out.println("writeNewUserToFile");
-		System.out.println(context.getRealPath(ACCOUNTDB));
-
+	private static final String URL="jdbc:mysql://localhost:3306/rememberthisdb";
+	
+	
+	public PersistenceDAOImpl() {
 		try {
-			PrintWriter pw = new PrintWriter(new FileWriter(context.getRealPath(ACCOUNTDB), true));
-			pw.println(user.getEmail()+"|"+user.getPassword());
-			pw.close();
-		} catch (IOException e) {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
+			System.err.println("Error loading MySQL Driver!!!");
 		}
 	}
-
 	@Override
-	public List<Memo> loadFromFile(String email) {
-		InputStream is = context.getResourceAsStream(ACCOUNTDATA);
-		if(is == null){
-			System.out.println("is is null");
-		}
-		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+	public void writeToDb(User user, WebApplicationContext wac) {
+		// TODO Auto-generated method stub
 		
-		String line;
-		List<Memo> memoLoad = new ArrayList<>();
-		try {
-			while((line = br.readLine())!=null){
-				int counter = 0;
-				String title = "";
-				String content = "";
-				String[] lineSplit = line.split("\\|");
-				System.out.println(Arrays.deepToString(lineSplit));
-				if(lineSplit[0].trim().equals(email)){
-					for (String string : lineSplit) {
-						if(string.equals(email)){
-							continue;
-						}
-						counter++;
-						if(counter % 2 != 0){
-							title=string;
-						} else{
-							content=string;
-						}
-						if(counter == 2){
-						memoLoad.add(new Memo(title, content));
-						}
-					}
-				}
-				
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return memoLoad;
 	}
-
+	@Override
+	public List<Memo> loadFromDb(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	@Override
 	public boolean doesThisAccountExist(User user, WebApplicationContext wac) {
-		if(wac == null){
-			System.out.println("wac is null");
-		}
-		InputStream is = wac.getServletContext().getResourceAsStream(ACCOUNTDB);
-		BufferedReader br = new BufferedReader(new InputStreamReader(is));
-		
-		String line;
-		
-		try {
-			while((line = br.readLine())!=null){
-				String[] lineSplit = line.split("\\|");
-				System.out.println(lineSplit[0]);
-				if(lineSplit[0].trim().equals(user.getEmail())){
-					return true;
-				}
-				
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		// TODO Auto-generated method stub
 		return false;
 	}
-
 	@Override
 	public boolean doesThisPasswordMatch(User user, WebApplicationContext wac) {
-		InputStream is = wac.getServletContext().getResourceAsStream(ACCOUNTDB);
-		BufferedReader br = new BufferedReader(new InputStreamReader(is));
-		
-		String line;
-		
-		try {
-			while((line = br.readLine())!=null){
-				String[] lineSplit = line.split("\\|");
-				System.out.println(Arrays.deepToString(lineSplit));
-				if(lineSplit[1].trim().equals(user.getPassword())){
-					return true;
-				}
-				
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		// TODO Auto-generated method stub
 		return false;
 	}
-
 	@Override
-	public void writeToFile(User user, WebApplicationContext wac) {
-		
-		InputStream is = wac.getServletContext().getResourceAsStream(ACCOUNTDB);
-		BufferedReader br = new BufferedReader(new InputStreamReader(is));
-		
-		String line;
-		
-		try {
-			PrintWriter pw = new PrintWriter(new FileWriter(context.getRealPath(ACCOUNTDATA), true));
-//			while((line = br.readLine())!=null){
-//				String[] lineSplit = line.split("\\|");
-//				if(lineSplit[0].equals(user.getEmail())){
-//					pw.close();
-//					
-//				}
-//			} 
-	
-			pw.println(user.getEmail()+"|"+user.getMemosInSavableFormat());
-			pw.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public void writeNewUserToDB(User user) {
+		// TODO Auto-generated method stub
 		
 	}
 
-
-
-
 	
-
 }
